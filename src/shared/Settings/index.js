da557f0082088a512727ea93d38adac46e0ba30f
Tabrez Ansari <tabrezsvce@gmail.com>
2023-02-17 18:28:47 +0530

    feat: added some basic ui

--- src/shared/Settings/index.js
+++ src/shared/Settings/index.js
@@ -2,6 +2,11 @@ import { Divider, List, ListItemButton, ListItemIcon, ListItemText } from "@mui/
 import React from "react"
 import InboxIcon from '@mui/icons-material/Inbox';
 import DraftsIcon from '@mui/icons-material/Drafts';
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+import CorporateFareRoundedIcon from '@mui/icons-material/CorporateFareRounded';
+import PeopleRoundedIcon from '@mui/icons-material/PeopleRounded';
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import { Input } from "antd";
 function Settings(props) {
     const [selectedIndex, setSelectedIndex] = React.useState(1);
 
@@ -16,50 +21,70 @@ function Settings(props) {
             </div>
             <Divider />
 
+            <div className="w-full flex">
 
-            <div className="w-1/4">
-                <List component="nav" aria-label="main mailbox folders">
-                    <ListItemButton
-                        selected={selectedIndex === 0}
-                        onClick={(event) => handleListItemClick(event, 0)}
-                    >
-                        <ListItemIcon>
-                            <InboxIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Inbox" />
-                    </ListItemButton>
-                    <ListItemButton
-                        selected={selectedIndex === 1}
-                        onClick={(event) => handleListItemClick(event, 1)}
-                    >
-                        <ListItemIcon>
-                            <DraftsIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Drafts" />
-                    </ListItemButton>
 
-                    <ListItemButton
-                        selected={selectedIndex === 4}
-                        onClick={(event) => handleListItemClick(event, 1)}
-                    >
-                        <ListItemIcon>
-                            <DraftsIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Drafts" />
-                    </ListItemButton>
+                <div className="w-1/4 border-r-2">
+                    <List component="nav" aria-label="main mailbox folders">
+                        <ListItemButton
+                            selected={selectedIndex === 0}
+                            onClick={(event) => handleListItemClick(event, 0)}
+                        >
+                            <ListItemIcon style={{ minWidth: 38 }}>
+                                <PersonRoundedIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Basic Profile" />
+                        </ListItemButton>
+                        <ListItemButton
+                            selected={selectedIndex === 1}
+                            onClick={(event) => handleListItemClick(event, 1)}
+                        >
+                            <ListItemIcon style={{ minWidth: 38 }}>
+                                <CorporateFareRoundedIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Company" />
+                        </ListItemButton>
 
-                    <ListItemButton
-                        selected={selectedIndex === 3}
-                        onClick={(event) => handleListItemClick(event, 1)}
-                    >
-                        <ListItemIcon>
-                            <DraftsIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Drafts" />
-                    </ListItemButton>
-                </List>
+                        <ListItemButton
+                            selected={selectedIndex === 4}
+                            onClick={(event) => handleListItemClick(event, 1)}
+                        >
+                            <ListItemIcon style={{ minWidth: 38 }}>
+                                <PeopleRoundedIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Members" />
+                        </ListItemButton>
 
+                        <ListItemButton
+                            selected={selectedIndex === 3}
+                            onClick={(event) => handleListItemClick(event, 1)}
+                        >
+                            <ListItemIcon style={{ minWidth: 38 }}>
+                                <SettingsRoundedIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Settings" />
+                        </ListItemButton>
+                    </List>
+
+                </div>
+                <div className="w-full p-4 flex flex-wrap">
+                    <div className="input-element w-1/2">
+                        <Input placeholder="First Name" style={{ height: '2.4rem', width: '95%' }} />
+                    </div>
+                    <div className="input-element w-1/2">
+                        <Input placeholder="Last Name" style={{ height: '2.4rem', width: '95%' }} />
+                    </div>
+
+                    <div className="input-element w-1/2">
+                        <Input placeholder="Mobile" style={{ height: '2.4rem', width: '95%' }} />
+                    </div>
+
+                    <div className="input-element w-1/2">
+                        <Input placeholder="Company" style={{ height: '2.4rem', width: '95%' }} />
+                    </div>
+                </div>
             </div>
+
 
         </div>
 
